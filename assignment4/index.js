7e058243f84079c18da92ef10dac3251fb673206
abhishek-aringale-bbi <141905269+abhishek-aringale-bbi@users.noreply.github.com>
2023-08-10 19:00:29 +0530

    Update index.js

--- assignment4/index.js
+++ assignment4/index.js
@@ -1,1 +1,21 @@
+const first=document.getElementById("F");
+const second=document.getElementById('S');
+const third=document.getElementById('T');
 
+first.addEventListener("click",function(event){
+    first.style.backgroundColor='black';
+    second.style.backgroundColor='white';
+    third.style.backgroundColor='white';
+});
+
+second.addEventListener("click",function(event){
+    first.style.backgroundColor='white';
+    second.style.backgroundColor='black';
+    third.style.backgroundColor='white';
+});
+
+third.addEventListener("click",function(event){
+    first.style.backgroundColor='white';
+    second.style.backgroundColor='white';
+    third.style.backgroundColor='black';
+});
