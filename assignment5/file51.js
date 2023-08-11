70e78d2665c422128f3e66fc78681a46b8776b04
abhishek-aringale-bbi <141905269+abhishek-aringale-bbi@users.noreply.github.com>
2023-08-11 13:23:30 +0530

    Update file51.js

--- assignment5/file51.js
+++ assignment5/file51.js
@@ -11,6 +11,18 @@ class Person{
 
 }
 
+class Teacher extends Person{
+    constructor(name,age,gender,subject){
+        super(name,age,gender);
+        this.subject=subject;
+    }
+
+    introduce(){
+        super.introduce();
+        console.log(`i teach ${this.subject}`)
+    }
+}
+
 class Student extends Person {
     constructor(name, age, gender, studentid) {
         super(name, age, gender);
@@ -23,17 +35,7 @@ class Student extends Person {
     }
 }
 
-class Teacher extends Person{
-    constructor(name,age,gender,subject){
-        super(name,age,gender);
-        this.subject=subject;
-    }
 
-    introduce(){
-        super.introduce();
-        console.log(`i teach ${this.subject}`)
-    }
-}
 
 
 const person1=new Person("abhishek",22,"male");
